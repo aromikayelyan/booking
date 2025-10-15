0be19d6a81d5ed17461688d767a04fb419194385
aromikayelyan <aromiq1@gmail.com>
2025-10-15 23:51:10 +0300

    create readme file

--- server/routes/bookingRoute.js
+++ server/routes/bookingRoute.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { User, Event, Booking } from "../models/index.js";
+import booking from "../models/booking.js";
 // import user from "../models/user.js";
 // import booking from "../models/booking.js";
 // import event from "../models/event.js";
@@ -20,6 +21,45 @@ router.get('/', async (req, res)=>{
 })
 
 
+router.post('/reserve', async (req, res)=>{
+    try {
+        const { event_id, user_id } = req.body
+
+
+        const event = await Event.findOne({where:{id: event_id}})
+        const user = await User.findOne({where:{id: user_id}})
+
+
+        if(!event){
+            return res.status(404).json({message: "Упс!!, Такого события нет в списке!!"})
+        }
+
+        if(!user){
+            return res.status(404).json({message: "Упс!!, Такого пользователя нет в списке!!"})
+        }
+
+
+        const HisAlreadyHave = await Booking.findOne({where:{event_id, user_id: user.uid}})
+
+        if(HisAlreadyHave){
+            return res.status(404).json({message: "Вы уже записаны на это меропреятие, двойноебронирование запрещено!!"})
+        }
+
+        const reserve = await Booking.create({
+            event_id,
+            user_id: user.uid
+        })
+
+
+        // console.log(event, user)
+
+
+        return res.status(200).json({mesage: "Регистрация на мероприятие прошла успешно!"})
+
+    } catch (e) {
+        console.log(e)
+    }
+})
 
 
 
